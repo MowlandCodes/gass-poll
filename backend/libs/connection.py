34980719b9ef35f987cac9b14257dee5abad7e3b
MowlandCodes <mowlandgaming@gmail.com>
2025-12-28 21:42:09 +0700

    backend: added environment variables

---
+++ backend/libs/connection.py
@@ -0,0 +1,14 @@
+from os import getenv
+
+from pymongo import MongoClient
+
+DB_USER = getenv("DB_USER", "user")
+DB_PASSWORD = getenv("DB_PASSWORD", "password")
+DB_HOST = getenv("DB_HOST", "localhost")
+DB_PORT = getenv("DB_PORT", "27017")
+DB_NAME = getenv("DB_NAME", "gass_poll")
+
+db_uri = f"mongodb://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/"
+conn = MongoClient(db_uri)
+
+db = conn[DB_NAME]
