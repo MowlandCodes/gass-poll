fe0b7e0a887156ad1bcbf46a89bfa25d11b18f29
MowlandCodes <mowlandgaming@gmail.com>
2026-01-01 16:59:45 +0700

    frontend: added logo in the footer

--- frontend/src/components/commons/Footer.tsx
+++ frontend/src/components/commons/Footer.tsx
@@ -1,20 +1,24 @@
-import { Heart } from "lucide-react";
+import { Motorbike } from "lucide-react";
 
 export default function Footer() {
   return (
-    <footer className="bg-slate-900 text-white py-12 border-t-4 border-orange-500">
+    <footer className="bg-slate-900 text-white py-8 border-t-4 border-orange-500">
       <div className="container mx-auto px-6 text-center">
-        <h2 className="text-2xl font-black mb-4 tracking-tighter">
-          GASS<span className="text-orange-500">POLL</span>
-        </h2>
+        <div className="flex items-center justify-center mb-8 space-x-2">
+          <div className="bg-orange-500 p-2 rounded-lg text-white transform group-hover:rotate-12 transition-transform duration-300 shadow-lg shadow-orange-500/20">
+            <Motorbike size={24} strokeWidth={2.5} />
+          </div>
+          <h2 className="text-2xl font-black tracking-tighter">
+            GASS<span className="text-orange-500">POLL</span>
+          </h2>
+        </div>
         <p className="text-slate-400 mb-8 max-w-md mx-auto">
           Solusi sewa motor anti ribet untuk mahasiswa santuy. Motor nyaman,
           harga teman.
         </p>
 
         <div className="text-sm text-slate-500 flex items-center justify-center gap-1 font-mono">
-          © {new Date().getFullYear()} Coded with{" "}
-          <Heart size={14} className="text-red-500 fill-red-500" /> by
+          © {new Date().getFullYear()} Created by
           <strong>DiBuffDoaOrtu</strong> Team
         </div>
       </div>
