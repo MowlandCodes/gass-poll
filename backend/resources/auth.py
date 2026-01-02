e18b3a5a43ff61c6e60ad5c777b5afea423d766b
CoKiW <razzfall09@gmail.com>
2026-01-02 22:11:25 +0700

    Backend: fixing auth

--- backend/resources/auth.py
+++ backend/resources/auth.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, request, current_app
+from html import parser
+from flask import Blueprint, request, current_app, reparse
 from flask_restful import Resource, Api
 from libs.connection import db
 import bcrypt
@@ -10,24 +11,45 @@ api_auth = Api(auth_bp)
 
 class register(Resource):
     def post(self):
-        data = request.get_json()
 
-        ## Validate input
+        parser = parser.RequestParser()
+
+        parser.add_argument('name', type=str, required=True, help="Name can't be blank")
+        parser.add_argument('email', type=str, required=True, help="Email can't be blank")
+        parser.add_argument('password', type=str, required=True, help="Password can't be blank")
+        parser.add_argument('phone_number', type=str, required=True, help="Phone number can't be blank")
+
+        data = parser.parse_args()
+
+        ## Validate name must be alphabetic
+        if not data.get('name').isalpha():
+            return {"message": "Name must contain only alphabetic characters."}, 400
+
+        ## Validate the password must more than 8 characters
+        if len(data.get('password')) < 8:
+            return {"message": "Password must be at least 8 characters long."}, 400
+
+        ## Validate phone number
+        if data.get('phone_number').startswith('+62'):
+            return {"message": "Phone number should not start with +62"}, 400
+
+        ## Validate email and password
         if not data.get('email') or not data.get('password'):
             return {"message": "Email and password are required."}, 400
         
         ## Check if user already exists in database via email
-        if db.users.find_one({'email': data['email']}):
+        if db.users.find_one({'email': data.get('email')}):
             return {"message": "User already exists."}, 400
         
         ## Make it secret password
-        hashed_password = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())
+        hashed_password = bcrypt.hashpw(data.get('password').encode('utf-8'), bcrypt.gensalt())
                                         
         new_user = {
             "name": data.get("name"),
             "password": hashed_password,
-            "email": data["email"],
-            "phone": data.get("phone_number"),
+            "email": data.get("email"),
+            "phone_number": data.get("phone_number"),
+            "role": "user"
         }
         
         db.users.insert_one(new_user)
@@ -39,10 +61,10 @@ class login(Resource):
         data = request.get_json()
         user = db.users.find_one({'email': data.get('email')})
 
-
-        if user and bcrypt.checkpw(data['password'].encode('utf-8'), user['password']):
+        if user and bcrypt.checkpw(data.get('password').encode('utf-8'), user.get('password')):
             token = jwt.encode({
-                'user_id': str(user['_id']),
+                'user_id': str(user.get('_id')),
+                'role': user.get('role') if user.get('role') else 'user',
                 'exp': datetime.utcnow() + timedelta(hours=1)
             }, current_app.config['SECRET_KEY'], algorithm='HS256')
 
--- backend/sheet.py
+++ backend/sheet.py
@@ -1,10 +1,32 @@
-from ast import Pass
+from email.mime import image
 from libs.connection import db
 import bcrypt
-from datetime import datetime, timedelta
+
 
 def hash_password(password):
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 def database_first():
-    pass
+    db.users.delete_many({})
+    db.motors.delete_many({})
+    db.rental_bills.delete_many({})
+
+    db.users.insert_one({
+        "username": "admin",
+        "email": "admin@example.com",
+        "password": hash_password("admin123"),
+        "phone": "1234567890",
+        "role": "admin"
+    })
+
+    db.motors.insert_one({
+        "name": "Yamaha R15",
+        "brand": "Yamaha",
+        "license_plate": "AB123CD",
+        "rental_price": 150000,
+        "status": "available",
+        "image": "https://placehold.co/600x400/png"
+    })
+
+if __name__ == "__main__":
+    database_first()
