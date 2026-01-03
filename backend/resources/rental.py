f2ccfbc8b1b2e177ba9e7ea70ccfd927f7352fae
Genzme <ziazarinr@gmail.com>
2026-01-03 23:21:55 +0700

    backend: update rental dan users (jwt extended feature)

--- backend/resources/rental.py
+++ backend/resources/rental.py
@@ -7,6 +7,7 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 
 from libs.connection import db
 from libs.utils import serialize_doc
+from parser.rental import rental_parser
 
 bp_rental = Blueprint("rental", __name__, url_prefix="/rental")
 rental_api = Api(bp_rental)
@@ -15,24 +16,60 @@ rental_api = Api(bp_rental)
 class Rental(Resource):
     @jwt_required()
     def post(self):
-        user_id = get_jwt_identity()
+        data = rental_parser.parse_args()
+        current_user_id = get_jwt_identity()
+        motor_id: str = data.get("motor_id")
+
+        try:
+            motor = db.motor.find_one({"_id": ObjectId(motor_id)})
+        except:
+            return {"message": "Invalid motor ID!"}, 400
+
+        if not motor:
+            return {"message": "Motor not found!"}, 404
+
+        if motor["status"] != "available":
+            return {"message": "Motor is not available for rent!"}, 400
+        
+        
+        duration_hours = int(data.get("duration_hours", 24))
+        rent_start = datetime.now()
+        rent_end = rent_start + timedelta(hours=duration_hours)
+        total_price = float(motor["rental_rate"]) * duration_hours
+
+        new_rental_bill = {
+            "user_id": ObjectId(current_user_id),
+            "motor_id": ObjectId(motor_id),
+            "rent_start": rent_start,
+            "rent_end": rent_end,
+            "created_at": datetime.now(),
+            "total_price": total_price,
+            "status": "ongoing"
+        }
+
+        result = db.rental_bills.insert_one(new_rental_bill)
+
+        db.motor.update_one(
+            {"_id": ObjectId(motor_id)}, {"$set": {"status": "rented"}}
+        )
+
+        return {
+            "message": "Rental bill created successfully.",
+            "rental_bill_id": str(result.inserted_id),
+        }, 201
 
     @jwt_required()
-    def get(self, current_user, user_id=None):
-        if not user_id:
-            try:
-                rental_bill = db.rental_bills.find_one({"user_id": ObjectId(user_id)})
-            except:
-                return {"message": "Invalid rental bill User!"}, 400
+    def get(self, user_id=None):
+        logged_in_user_id = get_jwt_identity()
 
-            if not rental_bill:
-                return {"message": "Rental bill not found!"}, 404
-
-            return serialize_doc(rental_bill), 200
+        if user_id:
+            if user_id != logged_in_user_id:
+                return {"message": "Unauthorized access!"}, 401
+            rentals = db.rental_bills.find({"user_id": ObjectId(user_id)})
         else:
-            rental_bills = db.rental_bills.find({})
-            bills_list = [serialize_doc(bill) for bill in rental_bills]
-            return bills_list, 200
+            rentals = db.rental_bills.find()
 
+        bills_list = [serialize_doc(bill) for bill in rentals]
+        return bills_list, 200
 
 rental_api.add_resource(Rental, "/", "/<string:user_id>")
--- backend/resources/users.py
+++ backend/resources/users.py
@@ -1,5 +1,5 @@
-from bson import ObjectId
-from flask import Blueprint, request
+from bson.objectid import ObjectId
+from flask import Blueprint
 from flask_restful import Api, Resource
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from libs.utils import serialize_doc
@@ -11,11 +11,14 @@ api = Api(bp_users)
 
 class userProfile(Resource):
     @jwt_required()
-    def get(self, current_user):
-        user_id = get_jwt_identity()
-        user = db.users.find_one({'_id': ObjectId(user_id)})
-        if not user:
+    def get(self):
+        current_user_id = get_jwt_identity()
+        user_data = db.users.find_one({'_id': ObjectId(current_user_id)})
+        
+        if not user_data:
             return {'message': 'User not found'}, 404
-        return serialize_doc(user), 200
-    
+        
+        user_data.pop('password', None)
+        return serialize_doc(user_data), 200
+
 api.add_resource(userProfile, '/')
