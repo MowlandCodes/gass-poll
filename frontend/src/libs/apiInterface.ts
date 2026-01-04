8ab4e0d7443cd86fc0da8217f8bf53c592793aae
MowlandCodes <mowlandgaming@gmail.com>
2026-01-04 20:39:56 +0700

    frontend: added api interface for Frontend

---
+++ frontend/src/libs/apiInterface.ts
@@ -0,0 +1,20 @@
+import axios, { type AxiosInstance } from "axios";
+
+export const backendApi: AxiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_BASE_API_URL,
+  timeout: 3000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Bakal ngambil token di localStorage kalo udh login
+backendApi.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
