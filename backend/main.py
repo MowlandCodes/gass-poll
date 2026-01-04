cde20491ec32265fd39abdea6616b52e23386f6e
MowlandCodes <mowlandgaming@gmail.com>
2026-01-04 23:53:20 +0700

    backend: fixed CORS

--- backend/main.py
+++ backend/main.py
@@ -1,40 +1,47 @@
 import os
+
+from dotenv import load_dotenv
 from flask import Flask
 from flask_cors import CORS
-from dotenv import load_dotenv
 from flask_jwt_extended import JWTManager
-load_dotenv()
 
 from libs.connection import db
-from resources.rental import bp_rental
-from resources.users import bp_users
 from resources.auth import auth_bp
 from resources.motor import bp_motor
+from resources.rental import bp_rental
+from resources.users import bp_users
+
+load_dotenv()
+
+FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:5173")
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('APP_SECRET_KEY')
-app.config['APP_PORT'] = int(os.getenv('APP_PORT', 5000))
-
-CORS(app) # Enabled CORS generally
-
-app.register_blueprint(bp_rental, url_prefix='/rental')
-app.register_blueprint(bp_users, url_prefix='/users')
-app.register_blueprint(auth_bp, url_prefix='/auth')
-app.register_blueprint(bp_motor, url_prefix='/motor')
+app.config["SECRET_KEY"] = os.getenv("APP_SECRET_KEY")
+app.config["APP_PORT"] = int(os.getenv("APP_PORT", 5000))
 
 jwt = JWTManager(app)
 
+
 @jwt.token_in_blocklist_loader
-def check_if_token_revoked(jwt_header, jwt_payload):
-    jti = jwt_payload['jti']
-    token = db.revoked_tokens.find_one({'jti': jti})
+def check_if_token_revoked(__header__, jwt_payload):
+    jti = jwt_payload["jti"]
+    token = db.token_blocklist.find_one({"jti": jti})
     return token is not None
 
+
 @jwt.revoked_token_loader
-def revoked_token_callback(jwt_header, jwt_payload):
-    return {'message': 'Token has been revoked'}, 401
+def revoked_token_callback(__header__, __jwt_payload__):
+    return {"message": "Unauthorized"}, 401
 
-if __name__ == '__main__':
+
+CORS(app, origins=[FRONTEND_URL], supports_credentials=True)
+
+app.register_blueprint(bp_rental, url_prefix="/rental")
+app.register_blueprint(bp_users, url_prefix="/users")
+app.register_blueprint(auth_bp, url_prefix="/auth")
+app.register_blueprint(bp_motor, url_prefix="/motor")
+
+
+if __name__ == "__main__":
     print(f"Starting server on port {app.config['APP_PORT']}...")
-    app.run(port=app.config['APP_PORT'], debug=True)
-
+    app.run(port=app.config["APP_PORT"], debug=True)
