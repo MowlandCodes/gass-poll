227550ca634bcd46f0d510fec5af959e8a595d83
MowlandCodes <mowlandgaming@gmail.com>
2026-01-08 23:34:36 +0700

    backend: added additional CORS Origins for development purposes

--- backend/main.py
+++ backend/main.py
@@ -14,10 +14,18 @@ from resources.users import bp_users
 load_dotenv()
 
 FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:5173")
+ADDITIONAL_ALLOWED_ORIGINS = os.getenv("ADDITIONAL_ALLOWED_ORIGINS", "").split(",")
+
+CORS_ORIGINS = [FRONTEND_URL]
+
+if ADDITIONAL_ALLOWED_ORIGINS != "" and ADDITIONAL_ALLOWED_ORIGINS != [""]:
+    CORS_ORIGINS.extend(ADDITIONAL_ALLOWED_ORIGINS)
+
 
 app = Flask(__name__, static_folder="./public", static_url_path="/public")
 app.config["SECRET_KEY"] = os.getenv("APP_SECRET_KEY")
 app.config["APP_PORT"] = int(os.getenv("APP_PORT", 5000))
+app.config["APP_HOST"] = os.getenv("APP_HOST", "localhost")
 
 jwt = JWTManager(app)
 
@@ -34,7 +42,7 @@ def revoked_token_callback(__header__, __jwt_payload__):
     return {"message": "Unauthorized"}, 401
 
 
-CORS(app, origins=[FRONTEND_URL], supports_credentials=True)
+CORS(app, origins=CORS_ORIGINS, supports_credentials=True, allow_private_network=True)
 
 app.register_blueprint(bp_rental, url_prefix="/rental")
 app.register_blueprint(bp_users, url_prefix="/users")
@@ -43,5 +51,6 @@ app.register_blueprint(bp_motor, url_prefix="/motor")
 
 
 if __name__ == "__main__":
-    print(f"Starting server on port {app.config['APP_PORT']}...")
-    app.run(port=app.config["APP_PORT"], debug=True)
+    print(f"✅ Allowed origins: {CORS_ORIGINS}")
+    print(f"🚀 Starting server on port {app.config['APP_PORT']}...")
+    app.run(host=app.config["APP_HOST"], port=app.config["APP_PORT"], debug=True)
